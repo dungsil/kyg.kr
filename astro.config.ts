45926c6f0fcc6d2a246f0fe9114124012aba4b95
김용건 (Kim Younggeon) <git+commit@younggeon.kim>
2024-09-23 21:04:58 +0900

    fix: redirect 시 파일로 떨어지는 문제 해결

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from 'astro/config'
+import redirects from './src/redirects.ts'
+
+const redirectEntries = Object.entries(redirects)
+  .map(([key, { href }]) => [`/${key}`, href])
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  redirects: Object.fromEntries(redirectEntries)
 })
